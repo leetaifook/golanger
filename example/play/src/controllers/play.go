7ae766fb3374866440cf16dc2b1e910fdccc0826
Jiang Bian <borderj@gmail.com>
2012-09-06 15:39:40 +0800

    add share but need fixed same bugs

--- example/play/src/controllers/play.go
+++ example/play/src/controllers/play.go
@@ -6,6 +6,7 @@
 package controllers
 
 import (
+    "fmt"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -14,7 +15,10 @@ import (
 )
 
 const (
+	PLAY = "http://play.golang.org/p/%s"
 	COMPILE = "http://play.golang.org/compile"
+	FMT = "http://play.golang.org/fmt"
+	SHARE = "http://play.golang.org/share"
 )
 
 type PagePlay struct {
@@ -31,6 +35,21 @@ func init() {
 }
 
 func (p *PagePlay) Index() {
+	if p.Request.Method != "GET" {
+		return
+	}
+    id := p.GET["p"]
+    if id == "" {
+        return
+    }
+    fmt.Println(id)
+    play := fmt.Sprintf(PLAY, id)
+
+    resp, _ := http.Get(play)
+	defer resp.Body.Close()
+
+	buf, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println(string(buf))
 }
 
 func (p *PagePlay) Compile() {
@@ -40,6 +59,45 @@ func (p *PagePlay) Compile() {
 
 	data := url.Values{"body": {strings.TrimSpace(p.POST["body"])}}
 	resp, err := http.PostForm(COMPILE, data)
+
+	defer resp.Body.Close()
+	if err != nil {
+		m := Compiled{"Error communicating with remote server.", ""}
+		ret, _ := json.Marshal(m)
+		p.ResponseWriter.Write(ret)
+		return
+	}
+	buf, _ := ioutil.ReadAll(resp.Body)
+	p.ResponseWriter.Write(buf)
+}
+
+func (p *PagePlay) Fmt() {
+	if p.Request.Method != "POST" {
+		return
+	}
+
+	data := url.Values{"body": {strings.TrimSpace(p.POST["body"])}}
+	resp, err := http.PostForm(FMT, data)
+
+	defer resp.Body.Close()
+	if err != nil {
+		m := Compiled{"Error communicating with remote server.", ""}
+		ret, _ := json.Marshal(m)
+		p.ResponseWriter.Write(ret)
+		return
+	}
+	buf, _ := ioutil.ReadAll(resp.Body)
+	p.ResponseWriter.Write(buf)
+}
+
+func (p *PagePlay) Share() {
+	if p.Request.Method != "POST" {
+		return
+	}
+
+    // FIXED
+    data := url.Values{"body":{strings.TrimSpace(p.POST["body"])}}
+	resp, err := http.PostForm(SHARE, data)
 
 	defer resp.Body.Close()
 	if err != nil {
