4bd5b54a4942025974371c79aa746bcbc9fdafbe
leetaifook <lee@leetaifook.com>
2012-09-08 00:08:16 +0800

    Reconstructed

--- example/chatroom/src/main.go
+++ example/chatroom/src/main.go
@@ -30,6 +30,7 @@ func main() {
 	http.Handle("/chat", websocket.Handler(BuildConnection))
 	go InitChatRoom()
 
+	App.AddHeader("Content-Type", "text/html; charset=utf-8")
 	App.HandleFavicon()
 	App.HandleStatic()
 	App.ListenAndServe(*addr)
