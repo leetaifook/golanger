580863e4be1cef09cdd4e7253ca933a7bc3ec9d0
leetaifook <lee@leetaifook.com>
2012-09-10 13:52:07 +0800

    Reconstructed

--- example/chatroom/src/static/theme/default/js/chatroom/global.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var pageName = document.location.pathname
-    if (pageName == "/") {
-        pageName = "/index.html"
-    }
-    
-    $(".nav-collapse > ul > li > a[href='" + pageName + "']").parent("li").addClass("active");
-});
