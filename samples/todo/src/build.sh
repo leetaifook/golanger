545d6e9ea57a6789bf202db02c4cca486fd227fb
leetaifook <lee@leetaifook.com>
2012-09-10 13:52:48 +0800

    Reconstructed

---
+++ samples/todo/src/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+APP="todo"
+ADDR=":8084"
+PWD=`pwd`/..
+GO_PWD=${PWD}/../..
+echo "Golanger Web Framework"
+echo "Golanger is a lightweight framework for writing web applications in Golang."
+export GOPATH=${GO_PWD}/framework:${PWD}/src/add-on:${PWD}
+cd ${PWD}/src
+
+if [ -f ${APP} ]; then
+    rm ${APP}
+fi
+
+echo "Building ${APP}"
+go build .
+
+if [ -f src ]; then
+    mv ./src ${APP}
+    echo "Runing ${APP}"
+    ./$APP -addr=${ADDR}
+fi
