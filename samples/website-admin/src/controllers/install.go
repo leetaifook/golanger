c228519f48f8dd2d847057cd1bb93a42c8510d2a
leetaifook <lee@leetaifook.com>
2012-09-12 15:50:37 +0800

    Reconstructed

--- samples/website-admin/src/controllers/install.go
+++ samples/website-admin/src/controllers/install.go
@@ -92,7 +92,7 @@ func (p *PageInstall) Index() {
 			p.Session.Clear(sessionSign)
 		}
 
-		p.RW.Write([]byte("安装成功...<br/>用户名:" + username + ",密码:" + password))
+		p.RW.Write([]byte("安装成功...<br/>用户名:"+username+",密码:"+password) + "<br/>")
 	}
 
 	http.Redirect(p.RW, p.R, "/login.html", http.StatusFound)
